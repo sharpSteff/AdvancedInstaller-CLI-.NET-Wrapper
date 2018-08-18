c13d5988cc6ff98a3e7e28ae4f4d485f220ec0a5
Stefan Kneidl <stefan.kneidl@alpine.de>
2018-08-18 13:45:40 +0200

    Initial commit

---
+++ AdvancedInstallerWrapper/AdvancedInstallerProject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace AdvancedInstaller
+{
+    public class AdvancedInstallerProject
+    {
+        /// <summary>
+        /// Creates an <see cref="AdvancedInstallerProject"/> instance to interact with the advanced installer CLI API
+        /// </summary>
+        /// <param name="pathToExe">Path to Advanced Installer executable</param>
+        public AdvancedInstallerProject(string pathToExe)
+        {
+            if (!File.Exists(pathToExe))
+            {
+                throw new FileNotFoundException("Advanced Installer executable not found!", pathToExe);
+            }
+
+        }
+
+        public bool Open(string path)
+        {
+            return true;
+        }
+    }
+}
