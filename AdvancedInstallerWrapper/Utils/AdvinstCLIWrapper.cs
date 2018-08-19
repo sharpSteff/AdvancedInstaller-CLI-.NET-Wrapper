12f9203e726ccf6ca7b283b8368c510f2edd7fae
Stefan Kneidl <stefan.kneidl@alpine.de>
2018-08-19 11:14:16 +0200

    added unit test project CLIWrapper add additional functionalities

--- AdvancedInstallerWrapper/Utils/AdvinstCLIWrapper.cs
+++ AdvancedInstallerWrapper/Utils/AdvinstCLIWrapper.cs
@@ -40,6 +40,30 @@ namespace AdvancedInstallerWrapper.Utils
             return CheckForError(result);
         }
 
+        /// <summary>
+        /// Change the package output type
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="projectFilePath"></param>
+        /// <param name="packageType"> MsiInside, MsiOutside, ExeInside, ExeOutside, and WebInstaller</param>
+        /// <param name="buildName"> is optional, if not specified, the command applies to the current build</param>
+        /// <param name="msiUrl">is required only for the WebInstaller package type</pa6ram>
+        /// <returns></returns>
+        public static bool ChangePackageOutputType(string path, string projectFilePath, PackageType packageType, string buildName = "", string msiUrl = "")
+        {
+            string command = string.Format("{0} {1} \"{2}\" {3} {4} {5} {6}", 
+                ComName, 
+                "/edit", 
+                projectFilePath, 
+                "/SetOutputType", 
+                packageType.ToString(), 
+                string.IsNullOrWhiteSpace(buildName) ? "" : "-buildname " + buildName,
+                (packageType == PackageType.WebInstaller) ? "-msi_url " + "\""+ msiUrl +"\"" : "");
+
+            var result = ShellCommandExecutor.ExecuteSynchronous(path, command);
+
+            return CheckForError(result);
+        }
 
         private static bool CheckForError(string result)
         {
---
+++ AdvancedInstallerWrapper/Utils/PackageType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedInstallerWrapper.Utils
+{
+    public enum PackageType
+    {
+        MsiInside,
+        MsiOutside,
+        ExeInside,
+        ExeOutside,
+        WebInstaller
+    }
+}
